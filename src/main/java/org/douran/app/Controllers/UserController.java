12845c3265a080b5ad59773f54727eab6f72df78
aryan-zargar <aryan.zargar90@gmail.com>
2026-02-22 16:43:30 +0330

    edited info on login service

--- src/main/java/org/douran/app/Controllers/UserController.java
+++ src/main/java/org/douran/app/Controllers/UserController.java
@@ -22,8 +22,6 @@ public class UserController {
     @Autowired
     private UserService userService;
     private Encryption encryptor = new Encryption();
-
-
     @PostMapping("/login")
     public ResponseEntity<String> Login(@RequestBody User user) throws NoSuchAlgorithmException {
         String username = user.getUsername();
