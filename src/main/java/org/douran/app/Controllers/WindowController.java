3fb4ee2e9c2b450bf9c97f05179d7ff2c31fa60c
aryan-zargar <aryan.zargar90@gmail.com>
2026-02-24 14:33:06 +0330

    added Controller for AD_WINDOW

---
+++ src/main/java/org/douran/app/Controllers/WindowController.java
@@ -0,0 +1,34 @@
+package org.douran.app.Controllers;
+
+import org.douran.app.Models.Book;
+import org.douran.app.Models.Client;
+import org.douran.app.Models.Window;
+import org.douran.app.Services.BookService;
+import org.douran.app.Services.ClientService;
+import org.douran.app.Services.WindowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/windows")
+@CrossOrigin(origins = "*")
+public class WindowController {
+    public WindowController(WindowService windowService) {
+        this.windowService = windowService;
+    }
+    private WindowService windowService;
+
+    @GetMapping
+    public ResponseEntity<List<Window>> getAllWindows() {
+        List<Window> clients = windowService.getAllWindows();
+        return ResponseEntity.ok(clients);
+    }
+    @GetMapping("/{ClientId}")
+    public ResponseEntity<Window> getWindowByClientId(@PathVariable Long ClientId) {
+        Window window = windowService.getWindowByClientId(ClientId);
+        return ResponseEntity.ok(window);
+    }
+}
